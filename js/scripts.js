8232640dcc50fc67162d6c2d4a8eaeaf95455d36
Blake Womack and Megan Warnock <megan@>
2017-02-23 14:13:40 -0800

    add functionality for determining tens spot and displaying.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
-var onesArray = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+var romanNumbIndex = [ ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+                       ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+                       ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+                       ["M", "MM", "MMM", "MMMM"] ]
+
 var index;
 //backend logic
 var RomanNumOnes = function(textInput) {
-  //textInput = )
+  var inputLength = textInput.length;
+  alert(inputLength);
+  var index = textInput;
   alert(textInput);
-  if (index = textInput - 1) {
-    return onesArray[index];
+  if (inputLength = 1) {
+    return romanNumbIndex[0][index - 1];
   //} else {
-
   }
-
 }
+
+
+
+
+
 
 //front end logic
 $(function() {
   $("#formOne").submit(function() {
-
-  event.preventDefault();
-  var numbInput = $("input#textInput").val();
-  var result = RomanNumOnes(numbInput);
-  console.log(onesArray);
-  $("#output").text(result);
-
-
+    event.preventDefault();
+    var numbInput = $("input#textInput").val();
+    var result = RomanNumOnes(numbInput);
+    console.log(romanNumbIndex);
+    //$("#output").text(romanNumbIndex[0][3]);
+    $("#output").text(result);
   });
 });
